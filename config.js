9e886f5ca01f76c775d2cbe74557b54241acc7dd
Christopher Marshall <thoughtpalettechris@gmail.com>
2015-05-11 13:40:37 -0500

    Updated prod url in config

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://thoughts.thoughtpalette.com',
+        url: 'http://thoughtpalette.com/thoughts',
         mail: {},
         database: {
             client: 'sqlite3',
